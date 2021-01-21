f9bfa8837913c86a2d2fa9204b54e13f49af5603
Anilkumrswain140@ <Anilkumrswain140@gmail.com>
2021-01-21 18:29:11 +0530

    commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(
   )
   app.use(express.json())
   app.all("/*", function(req, res, next){
-    res.header('Access-Control-Allow-Origin', 'https://webauthapi.herokuapp.com/');
+    res.header('Access-Control-Allow-Origin', 'https://ionic.centroxy.com:8100/');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     next();
